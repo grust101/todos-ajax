2d669d36e4304f315362f55f6571e964400b2163
Eleanor Roosevelt <georgia.rust@gmail.com>
2016-07-20 10:33:33 -0700

    AJAX Lecture

--- app/controllers/todos.rb
+++ app/controllers/todos.rb
@@ -9,10 +9,30 @@ end
 
 get '/todos/new' do
   @todo = Todo.new
-  if request.xhr?
-    erb :new, layout: false
+  if request.xhr? ## AJAX Request
+    erb :new, layout: false #send the form but dont send the layout because it's already been loaded
   else
-    erb :new
+   #Sending all the data HTML CSS AND JS for new form
+   erb :new
+ end
+end
+
+post '/todos' do 
+p params
+  @todo = Todo.new(params[:todos])
+  if @todo.save
+    if request.xhr?
+      p '=' * 10
+      content_type :json
+      data = {new_todo: @todo}.to_json
+      # erb :new, layout: false
+      p @todo
+    else
+    end
+
+    redirect "/todos"
+  else
+    redirect "/todos/new" 
   end
 end
 
--- public/js/application.js
+++ public/js/application.js
@@ -1,5 +1,56 @@
 $(document).ready(function() {
-  console.log('READY');
+	console.log('READY');
+
+	getNewTodosForm();
+	postNewTodo();
+
 
 });
 
+function getNewTodosForm(){
+// console.log("inside new form function");
+$('#new-todo-form').on('click', function(event){
+	event.preventDefault();
+
+	// console.log('event', event)
+
+	$.ajax({ 
+		url: '/todos/new',
+		method: 'GET'
+	})
+	.done(function(serverData){
+		// console.log(serverData);
+		// hide the link for new form
+		$('#new-todo-form').hide();
+		// show the form
+		$('#new-todo').append(serverData);
+	})
+	.fail(function(serverData){
+		console.log('failing');
+	})
+})
+};
+
+
+function postNewTodo(){
+	$('#new-todo').on('submit', function(event){
+		event.preventDefault();
+		console.log(this)
+		var formData = $('#new-todo form').serialize();
+		// console.log(formData)
+
+
+		$.ajax ({
+			url: '/todos', 
+			method: 'POST',
+			data: formData
+		})	
+		.done(function(serverData){
+			console.log('Success', serverData)
+		})
+		.fail(function(serverData){
+			console.log('Failing')
+		})
+	})
+
+}
